7dd2161f2de54167a1e4b34d3d470ebda1223b0a
awynkoop <wynkoopandre1@gmail.com>
2025-04-09 13:18:32 -0400

    adding functionality for player to move around in game space

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -6,11 +7,32 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT),vsync=60)
     
-    while True:
-        for event in pygame.event.get():
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    #creating group
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable,drawable)
+    
+    #spawns player in the middle of the screen
+    player1 = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
+    
+    
+    while True: 
+        for event in pygame.event.get(): #checking if game is quit
             if event.type == pygame.QUIT:
                 return
+        
+        player1.update(dt)
         screen.fill("black")
+        player1.draw(screen)
+        pygame.display.flip()
+        
+        #pauses game loop for 1/60th of a second
+        tick =  clock.tick(60)
+        dt = tick / 1000
+        
     
     #print("Starting Asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
